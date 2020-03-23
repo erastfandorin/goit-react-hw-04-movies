62cdac91c53fcbaf12f6f9b6b4e5ff62c3c8ae36
Alexander Khomenko <homenkosasha13@gmail.com>
2020-03-23 15:49:41 +0200

    Updates

---
+++ precache-manifest.8424abeb5f529b85f99ed926026d5d18.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ef9c8b48a67be63eced3eb9bec35b2",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "de25767ec041aa1a81d9",
+    "url": "/goit-react-hw-04-movies/static/css/cast.cb88c1b6.chunk.css"
+  },
+  {
+    "revision": "a32d18933d351445541d",
+    "url": "/goit-react-hw-04-movies/static/css/main.af881a28.chunk.css"
+  },
+  {
+    "revision": "3e93d28e1d5b59f6852b",
+    "url": "/goit-react-hw-04-movies/static/css/movies-details-page.6342cd89.chunk.css"
+  },
+  {
+    "revision": "5118aae4710273989f2f",
+    "url": "/goit-react-hw-04-movies/static/css/movies-page.424469e0.chunk.css"
+  },
+  {
+    "revision": "9e316b5dbb1995752d69",
+    "url": "/goit-react-hw-04-movies/static/css/reviews.f58dec69.chunk.css"
+  },
+  {
+    "revision": "9334b9c7cf37806547d1",
+    "url": "/goit-react-hw-04-movies/static/js/7.6237a901.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/goit-react-hw-04-movies/static/js/7.6237a901.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de25767ec041aa1a81d9",
+    "url": "/goit-react-hw-04-movies/static/js/cast.851414b4.chunk.js"
+  },
+  {
+    "revision": "99e9b86bf2e949032402",
+    "url": "/goit-react-hw-04-movies/static/js/home-page.8287b21a.chunk.js"
+  },
+  {
+    "revision": "a32d18933d351445541d",
+    "url": "/goit-react-hw-04-movies/static/js/main.b7e78c1b.chunk.js"
+  },
+  {
+    "revision": "3e93d28e1d5b59f6852b",
+    "url": "/goit-react-hw-04-movies/static/js/movies-details-page.50d74d43.chunk.js"
+  },
+  {
+    "revision": "5118aae4710273989f2f",
+    "url": "/goit-react-hw-04-movies/static/js/movies-page.167295bd.chunk.js"
+  },
+  {
+    "revision": "9e316b5dbb1995752d69",
+    "url": "/goit-react-hw-04-movies/static/js/reviews.352bd6a4.chunk.js"
+  },
+  {
+    "revision": "dcae68dc033d0c4c7ade",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.4fa69d7c.js"
+  }
+]);
